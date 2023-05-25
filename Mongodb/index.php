4c14da70e1bafec2217717d9e3395cbf591a219e
saadaziz0014 <saadaziz0014@gmail.com>
2023-05-25 16:51:30 +0500

    mongodb connection

---
+++ Mongodb/index.php
@@ -0,0 +1,23 @@
+<?php
+$connect = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
+echo "Connected successfully";
+
+$db = $connect->ecommercex;
+
+echo 'Database selected';
+
+$customer = $db->createCollection("customer");
+echo "Customer Collection created succsessfully";
+
+
+$product = $db->createCollection("product");
+echo "Product Collection created succsessfully";
+
+$order = $db->createCollection("order");
+echo "Orders Collection created succsessfully";
+
+$order_details = $db->createCollection("order_details");
+echo "Order_details Collection created succsessfully";
+
+?>
